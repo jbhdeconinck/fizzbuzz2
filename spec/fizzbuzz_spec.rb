0fffd9fefa8f4946cb2ca0e9f92109a278db4f97
Maciej Kurek <maciejk77@gmail.com>
2015-10-07 21:12:55 +0100

    Second test failing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,19 @@
 require 'fizzbuzz'
 
 describe "FizzBuzz" do
+ 
   context 'knows that a number is divisible by' do
     it '3' do
       expect(is_divisible_by_three?(3)).to be_truthy
+    end
+    it '5' do
+      expect(is_divisible_by_five?(5)).to be_truthy
+    end
+  end
+
+  context 'knows that a number is NOT divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(1)).not_to be_truthy
     end
   end
 end
