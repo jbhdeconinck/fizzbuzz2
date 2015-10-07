d478a335c622d88c5dc16ba477838782ad52a7b9
jbhdeconinck <jbhdeconinck.gmail.com>
2015-10-07 21:20:43 +0100

    third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require 'fizzbuzz'
 
 describe "FizzBuzz" do
- 
+
   context 'knows that a number is divisible by' do
     it '3' do
       expect(is_divisible_by_three?(3)).to be_truthy
@@ -9,11 +9,18 @@ describe "FizzBuzz" do
     it '5' do
       expect(is_divisible_by_five?(5)).to be_truthy
     end
+    it '15' do
+      expect(is_divisible_by_fifteen?(15)).to be_truthy
+    end
   end
 
   context 'knows that a number is NOT divisible by' do
     it '3' do
       expect(is_divisible_by_three?(1)).not_to be_truthy
     end
+    it '5' do
+      expect(is_divisible_by_five?(1)).not_to be_truthy
+    end
+
   end
 end
